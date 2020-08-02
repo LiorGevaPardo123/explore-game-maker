d1ac1960c0f390edd5c8b814deb819a49b678e80
LiorGevaPardo12 <snailior2212@gmail.com>
2020-08-02 10:06:23 +0000

    .

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 "use strict";
-
 // initializing the game engine
 const gameEngine = new GameEngine();
 
+function AClassy(data)
+{
+  let arr = [];
+  for(let i = 0; i < (getScreenWidth())/3; i++)
+  {
+    for(let j = 0; i < (getScreenHeight())/4; i++)
+    {
+      arr.push([i, j]);
+    }
+  }
+
+  gameEngine.fillPixels(arr, 100, 0, 100, 255);  
+}
 /**
  * Run a single loop of the game.
  * @param {object}   data    the game state.
  */
 function mainLoop(data) {
-  console.log("hello");
+  
+
 }
 
 gameEngine.startMainLoop(mainLoop, {});
